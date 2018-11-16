d0da3d91826e97548d160332434cf78f57aca721
powell956 <powell956@gmail.com>
2018-11-16 12:31:20 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,7 +3,7 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-  name = value 
+  name = value
 end
 
 def argue(value)
@@ -15,13 +15,13 @@ def greeting(greeting, name)
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  expert
 end
 
 def pizza_party(type = "cheese")
-
+  type
 end
